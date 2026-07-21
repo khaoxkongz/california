744e763490c58d1032621088f8fb4248ad16fc63
Thanapat Makkawal <computer@null.local>
2026-07-21 10:40:21 +0700

    Initial commit from Create Next App

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite-plus";
+
+export default defineConfig({
+  lint: {
+    jsPlugins: [{ name: "vite-plus", specifier: "vite-plus/oxlint-plugin" }],
+    rules: { "vite-plus/prefer-vite-plus-imports": "error" },
+    options: { typeAware: true, typeCheck: true },
+  },
+  fmt: {
+    ignorePatterns: [],
+    sortTailwindcss: {
+      stylesheet: "./src/app/globals.css",
+      functions: ["clsx", "cn"],
+    },
+  },
+});
